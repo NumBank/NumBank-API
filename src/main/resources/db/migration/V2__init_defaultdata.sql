6aadff5c53fe6f69e695e8b10838f232a20f4f7f
Daris <hei.raymond.2@gmail.com>
2024-03-21 19:07:30 +0300

    Add default data for category

---
+++ src/main/resources/db/migration/V2__init_defaultdata.sql
@@ -0,0 +1,5 @@
+INSERT INTO category VALUES 
+('3fa85f64-5717-4562-b3fc-2c963f66afa6', 'Salary', 'entry'),
+('bef4836e-781e-42df-ac87-919ba09f0f7f', 'Expense', 'out'),
+('05559ec5-8959-4890-927d-38a6128922d6', 'Billet', 'out'),
+('741985ae-0f8f-41f2-83b0-f9fc39964d2c', 'Business', 'entry');
