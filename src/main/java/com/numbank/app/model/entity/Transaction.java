1c8b883ce8122d2af6b90111690c2b156bf8a835
Daris02 <hei.raymond.2@gmail.com>
2024-03-27 15:36:58 +0300

    fix: change type of transaction date time

--- src/main/java/com/numbank/app/model/entity/Transaction.java
+++ src/main/java/com/numbank/app/model/entity/Transaction.java
@@ -1,6 +1,7 @@
 package com.numbank.app.model.entity;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
@@ -13,14 +14,14 @@ public class Transaction {
     private String id;
     private Double amount;
     private String label;
-    private Timestamp dateEffect;
-    private Timestamp saveDate;
+    private LocalDateTime dateEffect;
+    private LocalDateTime saveDate;
     private Boolean extern;
     private Boolean status;
     private String accountId;
     private String categoryId;
     
-    public Transaction(Double amount, String label, Timestamp dateEffect, Timestamp saveDate, Boolean extern,
+    public Transaction(Double amount, String label, LocalDateTime dateEffect, LocalDateTime saveDate, Boolean extern,
             Boolean status, String accountId, String categoryId) {
         this.amount = amount;
         this.label = label;
@@ -32,7 +33,7 @@ public class Transaction {
         this.accountId = accountId;
     }
 
-    public Transaction(Double amount, String label, Timestamp dateEffect, Timestamp saveDate, Boolean extern,
+    public Transaction(Double amount, String label, LocalDateTime dateEffect, LocalDateTime saveDate, Boolean extern,
             Boolean status, String categoryId) {
         this.amount = amount;
         this.label = label;
@@ -43,7 +44,7 @@ public class Transaction {
         this.categoryId = categoryId;
     }
 
-    public Transaction(String id, Double amount, String label, Timestamp dateEffect, Timestamp saveDate,
+    public Transaction(String id, Double amount, String label, LocalDateTime dateEffect, LocalDateTime saveDate,
             Boolean extern, Boolean status, String categoryId) {
         this.id = id;
         this.amount = amount;
--- src/main/java/com/numbank/app/model/entity/Transfert.java
+++ src/main/java/com/numbank/app/model/entity/Transfert.java
@@ -1,24 +1,25 @@
 package com.numbank.app.model.entity;
 
-import java.sql.Timestamp;
-
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class Transfert extends Transaction {
     private String accountIdSender;
     private String accountIdRecipient;
 
-    public Transfert(Double amount, String label, Timestamp dateEffect, Timestamp saveDate, Boolean extern,
-            Boolean status, String accountIdSender, String accountIdRecipient, String categoryId) {
+    public Transfert(Double amount, String label, LocalDateTime dateEffect, LocalDateTime saveDate, Boolean extern,
+                     Boolean status, String accountIdSender, String accountIdRecipient, String categoryId) {
         super(amount, label, dateEffect, saveDate, extern, status, categoryId);
         this.accountIdSender = accountIdSender;
         this.accountIdRecipient = accountIdRecipient;
     }
 
-    public Transfert(String id, Double amount, String label, Timestamp dateEffect, Timestamp saveDate, Boolean extern,
+    public Transfert(String id, Double amount, String label, LocalDateTime dateEffect, LocalDateTime saveDate, Boolean extern,
             Boolean status, String accountIdSender, String accountIdRecipient, String categoryId) {
         super(id, amount, label, dateEffect, saveDate, extern, status, categoryId);
         this.accountIdSender = accountIdSender;
