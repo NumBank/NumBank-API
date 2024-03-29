73bcfab55349b61648218822ebbea2e7044a1248
Daris02 <hei.raymond.2@gmail.com>
2024-03-29 19:06:48 +0300

    fix: optimise using response entity in rest controller

--- src/main/java/com/numbank/app/controller/AccountController.java
+++ src/main/java/com/numbank/app/controller/AccountController.java
@@ -10,12 +10,13 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
 
 @RestController
 @AllArgsConstructor
@@ -24,22 +25,38 @@ public class AccountController {
     private AccountService service;
 
     @GetMapping({"", "/"})
-    public List<Account> getAccounts() {
-        return service.getAll();
+    public ResponseEntity<List<Account>> getAccounts() {
+        List<Account> accounts = service.getAll();
+        if (accounts == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(accounts);
     }
 
     @GetMapping({"/{id}"})
-    public Account getAccountById(@PathVariable("id") String id) {
-        return service.getById(id);
+    public ResponseEntity<Account> getAccountById(@PathVariable("id") String id) {
+        Account account = service.getById(id);
+        if (account == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(account);
     }
     
     @PostMapping({"", "/"})
-    public List<Account> saveAccounts(@RequestBody List<Account> accounts) {
-        return service.saveAll(accounts);
+    public ResponseEntity<List<Account>> saveAccounts(@RequestBody List<Account> accounts) {
+        List<Account> accountSaved = service.saveAll(accounts);
+        if (accountSaved == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.ok(accountSaved);
     }
 
     @PutMapping({"/{id}"})
-    public Account updateAccount(@PathVariable String id, @RequestBody Account account) {
-        return service.update(account);
+    public ResponseEntity<Account> updateAccount(@PathVariable String id, @RequestBody Account account) {
+        Account accountUpdated = service.update(account);
+        if (accountUpdated == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(accountUpdated);
     }
 }
