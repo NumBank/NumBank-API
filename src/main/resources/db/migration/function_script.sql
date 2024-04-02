c3608c2e8c611571f4e6baad8cf9e31b8d04fc09
Sheddy00 <hei.sheddy.2@gmail.com>
2024-04-02 23:53:09 +0300

    chore: resloved bug on sql dashborad

--- src/main/resources/db/migration/function_script.sql
+++ src/main/resources/db/migration/function_script.sql
@@ -197,6 +197,8 @@ $$ LANGUAGE plpgsql;
 
 -- F2
 -- For the 1st chart (pie-chart) : Sum of amounts per category
+-- This function find the sum of amounts per category with given period
+-- The parameters in the dateEffect take the date of the choice
 SELECT
     c.name AS category,
     DATE_TRUNC('month', t.dateEffect) AS period,
@@ -210,8 +212,13 @@ WHERE t.dateEffect BETWEEN '2024-01-01' AND '2024-12-31'
 GROUP BY c.name, DATE_TRUNC('month', t.dateEffect);
 
 -- For the 2nd chart (chart of your choice) : Sum of expenses and income
+-- This function the sum of expenses and income with given date
 SELECT
-    SUM(CASE WHEN o.amount < 0 THEN o.amount ELSE 0 END) AS totalExpenses,
-    SUM(CASE WHEN o.amount >= 0 THEN o.amount ELSE 0 END) AS totalIncome
-FROM "Operation" o
-WHERE o.dateEffect BETWEEN :startDate AND :endDate;
+    SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END) AS totalExpensesTransfert,
+    SUM(CASE WHEN t.amount >= 0 THEN t.amount ELSE 0 END) AS totalIncomeTransfert,
+    SUM(CASE WHEN s.amount < 0 THEN s.amount ELSE 0 END) AS totalExpensesSupply,
+    SUM(CASE WHEN s.amount >= 0 THEN s.amount ELSE 0 END) AS totalIncomeSupply
+FROM "transfert" t
+         INNER JOIN "account" a ON a.id = t.id
+         INNER JOIN "supply" s ON s.id = a.id
+WHERE t.dateEffect BETWEEN '2024-01-01' AND '2024-12-31';
